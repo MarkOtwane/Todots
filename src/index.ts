51ba27a422427b9942c44452398800e2c467bcf5
KING DAVID <otwanemark254@gmail.com>
2025-05-31 08:29:37 +0300

    food app

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,41 @@
-let myName: string = "bob"
- 
-// exercise 1  explicit
-let numberWheels: number = 4
-let isStudent:boolean = false
-
-// custom type
-type Person = {
-    name: string,
-    age: number,
-    isStudent: boolean
+type Pizza = {
+    name: string
+    price: number
 }
 
-let person1 = {
-    name: "mark",
-    age:42,
-    isStudent:false
-}
+const menu = [
+    {name:"chicken", price: 8},
+    {name:"pork", price: 12},
+    {name:"beans", price: 23}
+]
 
-let person = {
-    name: "Joe",
-    age:21,
-    isStudent:true
-}
+let  cashInRegister = 100
+ let nextOrderId =  1
+ const orderQueue = [];
+
+ function addNewPizza(pizzaObj: Pizza){
+    menu.push(pizzaObj)
+ }
+
+ function placeOrder(pizzaName: string){
+    const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
+    if(!selectedPizza){
+        console.error(`${pizzaName} does not exist in the menu`);
+        return
+    }
+     cashInRegister = selectedPizza.price
+     const newOrder = {id:nextOrderId++, pizza: selectedPizza, status: 'ordered'}
+     orderQueue.push(newOrder);
+     return newOrder
+ }
+
+ function completedOrder(orderId: number){
+    const order = orderQueue.find(order=>order.id===orderId);
+    order.status = "completed"
+    return order;
+ }
+
+ addNewPizza({name: "meat", price: 23}),
+ addNewPizza({name: "kales", price: 10}),
+ addNewPizza({name: "cabbage", price: 15}),
+ addNewPizza({name: "managu", price: 12})
