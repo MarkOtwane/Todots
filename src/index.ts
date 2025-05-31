d002d5ea64379b425e0b47a7164b0a380c120ff0
KING DAVID <otwanemark254@gmail.com>
2025-05-31 08:07:33 +0300

    learn about custom types in javascript

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,24 @@
-type Task = {
-        title: string,
-        completed: boolean,
-        createdAt: Date,
-    }
+let myName: string = "bob"
+ 
+// exercise 1  explicit
+let numberWheels: number = 4
+let isStudent:boolean = false
 
-const list = document.querySelector<HTMLUListElement>('#list'); //only use for query selector
-const form = document.querySelector<HTMLFormElement>('#new-task-form');
-const input = document.querySelector<HTMLInputElement>('#new-task-title');
-
-const tasks: Task[] = loadTasks()  //array
-tasks.forEach(addListItem)
-
-form?.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    if(input?.value == '' || input?.value == null) return;
-
-    //create a tasks
-    const newTask: Task = {
-        title: input.value,
-        completed: false,
-        createdAt: new Date(),
-    }
-    tasks.push(newTask)
-
-    addListItem(newTask);
-    input.value = ''
-});
-
-function addListItem(task: Task){
-    const item = document.createElement('li');
-    const label = document.createElement('label');
-    const  checkbox = document.createElement("input");
-    checkbox.addEventListener('change', ()=>{
-    task.completed = checkbox.checked
-
-        // saveTask()
-        localStorage.setItem("TASKS", JSON.stringify(tasks))
-    })
-
-    checkbox.type = "checkbox"
-    checkbox.checked = task.completed
-    label.appendChild(checkbox);
-    label.appendChild(document.createTextNode(task.title))
-    item.append(label);
-    list?.append(item)
+// custom type
+type Person = {
+    name: string,
+    age: number,
+    isStudent: boolean
 }
 
-function loadTasks(): Task[]{
-    const taskJson = localStorage.getItem("TASKS");
-    if(taskJson == null) return[];
-    return JSON.parse(taskJson)
+let person1 = {
+    name: "mark",
+    age:42,
+    isStudent:false
+}
+
+let person = {
+    name: "Joe",
+    age:21,
+    isStudent:true
 }
