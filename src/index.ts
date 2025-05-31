cb5795cd661d7fbd72b47ec4ddf04aa223f943ce
KING DAVID <otwanemark254@gmail.com>
2025-05-31 09:22:21 +0300

    type narrowing

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
+type Order= {
+    id: number,
+    pizza: Pizza,
+    status: "ordered" | "completed"
+}
 type Pizza = {
+    id:number,
     name: string
     price: number
 }
 
-const menu = [
-    {name:"chicken", price: 8},
-    {name:"pork", price: 12},
-    {name:"beans", price: 23}
+const menu: Pizza[] = [
+    {id: 1,name:"chicken", price: 8},
+    {id:2,  name:"pork", price: 12},
+    {id:3, name:"beans", price: 23}
 ]
 
 let  cashInRegister = 100
  let nextOrderId =  1
- const orderQueue = [];
+ const orderQueue: Order[] = [];
 
  function addNewPizza(pizzaObj: Pizza){
     menu.push(pizzaObj)
@@ -24,18 +30,33 @@ let  cashInRegister = 100
         return
     }
      cashInRegister = selectedPizza.price
-     const newOrder = {id:nextOrderId++, pizza: selectedPizza, status: 'ordered'}
+     const newOrder: Order = {id:nextOrderId++, pizza: selectedPizza, status: "ordered"}
      orderQueue.push(newOrder);
      return newOrder
  }
 
  function completedOrder(orderId: number){
     const order = orderQueue.find(order=>order.id===orderId);
+    if(!order){
+        throw new Error(`Order with id ${orderId} not found`)
+        return
+    }
     order.status = "completed"
     return order;
  }
 
- addNewPizza({name: "meat", price: 23}),
- addNewPizza({name: "kales", price: 10}),
- addNewPizza({name: "cabbage", price: 15}),
- addNewPizza({name: "managu", price: 12})
+//  type narrowing 
+ function getPizzaDetails(identifier: string| number){
+    if(typeof identifier=== "string"){
+        return menu.find(pizza =>pizza.name.toLocaleLowerCase()=== identifier.toLocaleLowerCase())
+    }else{
+        return menu.find(pizza =>pizza.id=== identifier)
+    }
+ }
+
+ addNewPizza({id:12, name: "meat", price: 23}),menu
+ addNewPizza({id:25,name: "kales", price: 10}),
+ addNewPizza({ id:33,name: "cabbage", price: 15}),
+ addNewPizza({id:35, name: "managu", price: 12})
+
+ console.log(menu)
